e6d4808550395b9ac4788637265612a4f25ed032
Shouvik Saha <shouvik.saha@jupiter.money>
2020-02-04 15:28:15 +0530

    Try using postgres docker container.

--- jupiter-build/entrypoint.sh
+++ jupiter-build/entrypoint.sh
@@ -3,12 +3,13 @@
 export DEBIAN_FRONTEND=noninteractive
 
 apt update
-apt-get -y install openjdk-${INPUT_JAVA_VERSION}-jdk-headless postgresql-11
+apt-get -y install openjdk-${INPUT_JAVA_VERSION}-jdk-headless docker-ce-cli
 
 java -version
+docker -v
 
 echo "Starting postgres server"
-pg_ctlcluster 11 main start &
+docker run postgres:10.10 -e POSTGRES_PASSWORD=postgres -p 5432:5432
 
 echo 'Running gradle build.'
 
