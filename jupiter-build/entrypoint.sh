c455043e150a1e9184d0686b391d8eb61327e31d
Shouvik Saha <shouvik.saha@jupiter.money>
2020-02-04 19:15:49 +0530

    Inspect container.

--- jupiter-build/entrypoint.sh
+++ jupiter-build/entrypoint.sh
@@ -5,11 +5,11 @@ export DEBIAN_FRONTEND=noninteractive
 apk add openjdk${INPUT_JAVA_VERSION} postgresql-client
 
 echo "Starting postgres server"
-docker run -d -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=postgres -e POSTGRES_USER=postgres -p 5432:5432 postgres:10.10
+docker run --name postgres -d -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=postgres -e POSTGRES_USER=postgres -p 5432:5432 postgres:10.10
 
 docker ps -a
+docker inspect postgres
 
-ifconfig
 export PGPASSWORD='password'; psql -U postgres -d postgres -h 172.17.0.2 -p 5432
 
 echo 'Running gradle build.'
