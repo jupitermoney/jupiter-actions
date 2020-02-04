168fcae0525008ee4733967072c2d22f4147ff29
Shouvik Saha <shouvik.saha@jupiter.money>
2020-02-04 16:02:48 +0530

    Setting up docker installation.

--- jupiter-build/entrypoint.sh
+++ jupiter-build/entrypoint.sh
@@ -2,6 +2,14 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+echo "Setting up docker installation"
+apt update
+apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
 apt update
 apt-get -y install openjdk-${INPUT_JAVA_VERSION}-jdk-headless docker-ce
 
