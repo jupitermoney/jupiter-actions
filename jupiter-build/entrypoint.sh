27e370df8d493ec3630ba830034c44b182d7d9ac
Shouvik Saha <shouvik.saha@jupiter.money>
2020-02-04 18:36:53 +0530

    install iptables.

--- jupiter-build/entrypoint.sh
+++ jupiter-build/entrypoint.sh
@@ -4,7 +4,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 echo "Setting up docker installation"
 apt update
-apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common net-tools
+apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common iptables
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 bash -c 'echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu disco stable" > /etc/apt/sources.list.d/docker-ce.list'
 
@@ -18,7 +18,7 @@ echo "Starting postgres server"
 docker run -d -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=postgres -e POSTGRES_USER=postgres -p 5432:5432 postgres:10.10
 
 docker ps -a
-cat /etc/hosts
+iptables -L
 
 # export PGPASSWORD='password'; psql -U postgres -d postgres -h localhost -p 5432
 
